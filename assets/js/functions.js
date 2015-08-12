a3a0be1b9971e973a3beb6b22b6f59850b8d90e4
podyankee <you@gmail.com>
2015-08-12 14:23:29 +0300

    mentoring-section

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,31 @@
+$(function() {
+  mentoringBubbleClick();
+});
+
+function mentoringBubbleClick() {
+  $('.face').on('click', function () {
+	  
+	  var 
+	  	$this = $(this),
+	  	faceTop = $this.position().top,
+		  vertMath = -1 * (faceTop - 230);
+	  
+	  //console.log(vertMath);
+	  
+	  $this.parent().css('top', + vertMath +'px');
+    
+	  $this.addClass('has-bubble-open')
+	  .siblings().removeClass('has-bubble-open');
+	  
+  });
+	
+	
+}
+
+
 $(window).scroll(function(){
   youtubeVideoScroll();
+  startMentoring();
 });
 
 function  youtubeVideoScroll() {	
@@ -7,3 +33,16 @@ function  youtubeVideoScroll() {
 	
 	$('.video-strip').css('background-position','center -'+ wScroll+ 'px')
 }
+
+function  startMentoring() {	
+	
+	var wScroll = $(window).scrollTop();
+
+	if($('section.mentoring').offset().top - 200 < wScroll) {
+		$('.faces').addClass('launched');
+		setTimeout(function() {
+      $('.face:nth-child(3)').addClass('has-bubble-open') 
+    }, 400);
+	}
+	
+};
