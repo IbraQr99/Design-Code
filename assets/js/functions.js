1ca4405a7a64c26682edd38c0e0386fa18875d5c
podyankee <you@gmail.com>
2015-08-14 07:02:47 +0300

    mentoring mobile view

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -8,15 +8,25 @@ function mentoringBubbleClick() {
 	  var 
 	  	$this = $(this),
 	  	faceTop = $this.position().top,
-		  vertMath = -1 * (faceTop - 230);
+		vertMath = -1 * (faceTop - 230);
+	  	faceLeft = $this.position().left,
+		horzMath = 0 - faceLeft;
+	  
 	  
 	  //console.log(vertMath);
-	  
+	  if($(window).width() > 640){
 	  $this.parent().css('top', + vertMath +'px');
-    
-	  $this.addClass('has-bubble-open')
+	  } else {
+		  if($this.hasClass('back-btn')){
+			  mentoringNarrowStart();
+		  } else {
+		  $this.parent().css('left', + horzMath +'px');
+	  	}
+	  }
+	  	if(!$this.hasClass('back-btn')) {
+		  $this.addClass('has-bubble-open')
 	  .siblings().removeClass('has-bubble-open');
-	  
+		}
   });
 	
 	
@@ -38,11 +48,40 @@ function  startMentoring() {
 	
 	var wScroll = $(window).scrollTop();
 
-	if($('section.mentoring').offset().top - 200 < wScroll) {
-		$('.faces').addClass('launched');
-		setTimeout(function() {
-      $('.face:nth-child(3)').addClass('has-bubble-open') 
-    }, 400);
+	if($('section.mentoring').offset().top - 500 < wScroll ) {
+	   if($(window).width() > 640) {
+			$('.faces').addClass('launched');
+			if(!$('.face').hasClass('has-bubble-open')){
+				setTimeout(function() {
+      			$('.face:nth-child(3)').addClass('has-bubble-open') 
+    			}, 400);
+			}	
+		} else {
+			mentoringNarrowStart();
+		}
 	}
-	
-};
+}
+
+function mentoringNarrowStart() {
+  $('.faces').css({
+	  'top': '230px',
+	  'left': '0px'
+  });
+	$('.face').first().addClass('has-bubble-open').siblings().removeClass('has-bubble-open');
+}
+
+function mentoringWideStart() {
+  $('.faces').css({
+	  'top': '0px',
+	  'left': '0px'
+  });
+	$('.face:nth-child(3)').addClass('has-bubble-open').siblings().removeClass('has-bubble-open');
+}
+
+$(window).resize(function  () {
+  if($(window).width() > 640){
+	  mentoringWideStart();
+  } else {
+	  mentoringNarrowStart();
+  }
+});
