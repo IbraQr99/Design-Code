b04d0df862bc67f385859de97d2a061fc42f63fc
podyankee <you@gmail.com>
2015-09-15 05:17:12 +0300

    podcast section added

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,10 +1,21 @@
 $(function() {
   mentoringBubbleClick();
-  setInterval(function  () {articleTada();
+  setInterval(function  () {articleTada()
     
-  }, 4000)
+  }, 4000);
+
+	designBGStuff();
   	
 });
+
+function designBGStuff() {
+  $('.design-img-link').hover(function(){
+    $(this).parent().parent().css('background-color', $(this).data('color'));
+  }, function(){
+    // off > revert the color
+    $(this).parent().parent().css('background-color', $(this).parent().parent().data('orig-color'));
+  });
+}
 
 function articleTada () {
   var randNum = Math.floor(Math.random() * $('.article-thumb').length) +1;
@@ -57,7 +68,7 @@ function  youtubeVideoScroll() {
 function startArticles () {
   var wScroll = $(window).scrollTop();
 	if($('section.articles').offset().top - $(window).height()/2 < wScroll ) {
-		$('.article-thumb').each(function (i) {
+		$('.article-thumb').each(function ico(i) {
 		  setTimeout( function  (){
 		    
 			  $('.article-thumb').eq(i).addClass('is-visible');}, 200 * i);
